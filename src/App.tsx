c37587b74af78e380ecb9e7e3fbb198d2e7c2c0b
IamAsimKhalil <asimkhalil@outlook.com>
2023-01-12 12:58:49 +0500

    Updated the stylings

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,43 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 interface allCustomersPropTypes {
-  customerID: number;
+  customerId: string | number;
   customerName: string;
 }
 interface selectedCustomerpropTypes {
-  customerID: number;
+  customerId: number;
 }
 interface ordersPropsTypes {
-  orderID: number;
-  productID: number;
-  customerID: number;
+  orderId: number;
+  productId: number;
+  customerId: number;
   totalAmmount: number;
   orderDate: string;
   comments: string;
 }
 
 function App() {
-  const allCustInitialState = {
-    customerID: 0,
+  const allCustumersInitialState = {
+    customerId: "",
     customerName: "Select customer",
   };
-  const selectCustInitstate = {
-    customerID: 1000,
+  const selectedCustumerInitalState = {
+    customerId: 0,
   };
   const ordersInitialState = {
-    orderID: 100,
-    productID: 100,
-    customerID: 100,
+    orderId: 100,
+    productId: 100,
+    customerId: 100,
     totalAmmount: 0,
     orderDate: "not assigned",
     comments: "test",
   };
   const [allCustomers, setAllCustomers] = useState<allCustomersPropTypes[]>([
-    allCustInitialState,
+    allCustumersInitialState,
   ]);
   const [selectedCustomer, setSelectedCustomer] = useState<
     selectedCustomerpropTypes
-  >(selectCustInitstate);
+  >(selectedCustumerInitalState);
 
   const [orders, setOrders] = useState<ordersPropsTypes[]>([
     ordersInitialState,
@@ -51,42 +51,42 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        setAllCustomers([allCustInitialState, ...data]);
+        setAllCustomers([allCustumersInitialState, ...data]);
       });
   }, []);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/orders")
-      .then((response) => {
-        return response.json();
-      })
-      .then((orderData) => {
-        setOrders([...orderData]);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/api/orders")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((orderData) => {
+  //       setOrders([...orderData]);
+  //     });
+  // }, []);
 
   const handleSelectCustumer = (event: any) => {
-    setSelectedCustomer({ customerID: event.target.value });
+    setSelectedCustomer({ customerId: event.target.value });
   };
 
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+      <div className="first-page-container">
         <div className="record-controllers">
           <h5 className="h5-text">Customers</h5>
-          <div className="row-orderDivs">
+          <div className="row-order-divs">
             <div className="customer-placeholder">
               <div className="placeholder-arrangements">
                 <>
                   <select
                     className="customer-placeholder"
                     onChange={handleSelectCustumer}
-                    value={selectedCustomer.customerID}
+                    value={selectedCustomer.customerId}
                   >
                     {allCustomers.map((data) => (
                       <option
                         className="dropdown-button"
-                        value={data.customerID}
+                        value={data.customerId}
                       >
                         {data.customerName}
                       </option>
@@ -98,20 +98,19 @@ function App() {
             <button className="button">New Record</button>
           </div>
         </div>
-        {selectedCustomer.customerID !== 1000 && (
+        {selectedCustomer.customerId !== 0 && (
           <div className="order-history">
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <span style={{ marginLeft: "20px" }}>SR#</span>
-              <span style={{ marginLeft: "70px" }}>ORDERID</span>
-              <span style={{ marginLeft: "40px" }}>AMMOUNT</span>
-              <span style={{ marginLeft: "20px" }}>COMMENTS</span>
+            <div className="individual-records-container">
+              <span className="serial-number">SR#</span>
+              <span className="order-id">ORDERID</span>
+              <span className="ammount">AMMOUNT</span>
+              <span className="comments">COMMENTS</span>
             </div>
             <div className="record_container">
-              {/* (selectedCustomer.customerID===orders.customerID) && */}
               {orders.map((data) => (
-                <div style={{ display: "flex", flexDirection: "row" }}>
+                <div className="individual-records-container">
                   <div className="record-display-area">1</div>
-                  <div className="record-display-area">{data.orderID}</div>
+                  <div className="record-display-area">{data.orderId}</div>
                   <div className="record-display-area">{data.totalAmmount}</div>
                   <div className="record-display-area">{data.comments}</div>
                 </div>
